1b86c8a6d52119bb96f00e2ababcb75e290155ab
SayhanA <sayhanahmed05@gmail.com>
2023-05-03 04:51:33 +0600

    recipes data changed

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ app.get('/chefs', (req, res) => {
     res.send(chefs)
 })
 
+app.get('/chef/:chef', (req, res) => {
+    const name = req.params.chef;
+    const chef = chefs.find( chef => chef.name === name);
+    res.send(chef)
+})
+
 app.get('/recipes/:name', (req, res) => {
     const chef = req.params.name;
     const chefRecipes = recipes.filter( recipe => recipe.chef === chef)
